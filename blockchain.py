705847f8197af078c55aef34004de21a6dbbe9bf
g1gabyte ⠕ <72310196+g1gabyteDEV@users.noreply.github.com>
2023-04-21 11:10:58 +0100

    The blockchain now works

--- blockchain.py
+++ blockchain.py
@@ -10,10 +10,12 @@ class PotionBlock:
 
 
 initial_block = PotionBlock("-", ["-", "-"])
+managing_block = initial_block
 with open('transactions.txt', 'r') as input_file, open('block_hashes.txt', 'w') as output_file:
     for line1, line2 in zip(input_file, input_file):
         line1 = line1.strip()
         line2 = line2.strip()
         message = line1 + line2
-        potion_block = PotionBlock(initial_block.block_hash, [message])
-        output_file.write(potion_block.block_hash + '\n')
+        potion_block = PotionBlock(managing_block.block_hash, [message])
+        managing_block = potion_block
+        output_file.write(managing_block.block_hash + '\n')
